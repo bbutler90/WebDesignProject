9b297c7e6bb3413e03e8844d812e28dcb1ee4350
bbutler90 <brianbutler1990@gmail.com>
2024-04-15 20:04:42 +0100

    updated image type for performance

--- resources/scripts/script-original.js
+++ resources/scripts/script-original.js
@@ -45,7 +45,7 @@ function play(){
     
   images.forEach(function(image) {
     image.setAttribute('data-value', '0');
-    image.setAttribute('src', 'resources/images/game/question-mark.png'); 
+    image.setAttribute('src', 'resources/images/game/question-mark.webp'); 
   });
 
   //Create an Array with 1 (dog) and many 2(cats)
@@ -93,9 +93,9 @@ function setGameImages(element) {
   images.forEach(function(image) { 
       
     if(image.getAttribute('data-value') == "1"){
-      image.setAttribute('src', 'resources/images/game/dog-play.png');      
+      image.setAttribute('src', 'resources/images/game/dog-play.webp');      
     }else if(image.getAttribute('data-value') == "2"){
-      image.setAttribute('src', 'resources/images/game/cat-play.png');      
+      image.setAttribute('src', 'resources/images/game/cat-play.webp');      
     }else{
       document.getElementById("gameMessage").innerHTML = "<div class='alert alert-warning' role='alert'>You need to click on 'Play!' first.</div>";
     }
@@ -382,7 +382,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //Brian Butler - Script for the carousel on the avaialblepuppies.html page
 // Image paths
-var images = ['resources/images/carousel/puppies1.jpg', 'resources/images/carousel/puppies2.jpg', 'resources/images/carousel/puppies3.jpg'];
+var images = ['resources/images/carousel/puppies1.webp', 'resources/images/carousel/puppies2.webp', 'resources/images/carousel/puppies3.webp'];
 
 // Text for each image
 var texts = ['We are always excited when we have new litters - each one filled with adorable puppies ready to find their forever homes. Take a moment to read about adopting one of our puppies. Each of our puppies is given the utmost care and attention, ensuring they grow up healthy and happy. We work closely with veterinarians to ensure all our puppies are in the best of health before they go to their new homes. We also provide ongoing support and advice for new owners, helping you settle your new furry friend into your home. Our puppies are not just pets, they are family, and we are committed to helping each one find the perfect home. So why wait? Start the journey of adding a new member to your family today.', 'Our goal is to find each puppy a loving home where they can be part of the family. The dog’s welfare is our foremost consideration before a choice is made for placement. We believe that every puppy deserves a home where they will be loved, cared for, and treated as a member of the family. We take great care in assessing potential homes, ensuring that they can provide the necessary care and environment for our puppies. We prioritize the puppy\'s health, happiness, and well-being above all else. We also offer support and guidance to new owners, helping them understand the responsibilities that come with owning a dog.', 'We have new litters born regularly, and if you are interested in adopting a puppy, please fill out the form below and we will be in touch. The PDF questionnaire is available to download by clicking on the button below. We understand that adopting a puppy is a big decision, and we\'re here to help you every step of the way, We aim to provide all the necessary information to help you make an informed decision. Our puppies are raised with love and care, and we want to ensure they go to homes that can provide the same. So, if you\'re ready to open your heart and home to a new furry friend, fill out the form and start the adoption process today.'];
